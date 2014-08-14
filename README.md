fe99454101290778d327c182d60f7ac4e669bec9
danielballan <daniel.b.allan@gmail.com>
2014-08-14 09:58:16 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+ZVI Reader
+----------
+
+This is a plugin for [PIMS](https://github.com/soft-matter/pims) (Python Image Sequence) that reads AxioVision ZVI files.
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='zvi_reader',
+      version='0.1',
+      py_modules=['zvi_reader'],
+      install_requires=['PIMS', 'OleFileIO_PL', 'pillow'],
+      )
